82f2838e06a43ba2aa92a70b68e40733080e174d
Rangel <a22110098@ceti.mx>
2022-07-21 18:45:30 -0500

    Responsive Blob
    Casi me muero

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ $d.addEventListener("DOMContentLoaded", (e) => {
   map();
   getClap();
   getCountry();
+
+  console.log(innerWidth);
 });
 
 $d.addEventListener("click", (e) => {
@@ -19,5 +21,38 @@ $d.addEventListener("click", (e) => {
 $d.addEventListener("change", (e) => {
   console.log(e.target);
 
+  // location(Math.random(), Math.random());
+});
+
+addEventListener("resize", (e) => {
+  const $shape = $d.querySelector(".shape-title");
+  console.log(innerWidth);
+
+  /*if (innerHeight < 400) {
+    $shape.setAttribute("viewBox", "50 10 75 100");
+    return;
+  }
+
+  if (innerHeight < 400) {
+    $shape.setAttribute("viewBox", "50 10 75 100");
+    return;
+  }
+
+  if (innerHeight < 400) {
+    $shape.setAttribute("viewBox", "50 10 75 100");
+    return;
+  }
+
+  if (innerHeight < 400) {
+    $shape.setAttribute("viewBox", "50 10 75 100");
+    return;
+  }*/
+
+  if (innerHeight < 400) $shape.setAttribute("viewBox", "50 10 75 100");
+  else if (innerWidth < 600) $shape.setAttribute("viewBox", "50 10 50 100");
+  else if (innerWidth < 1000) $shape.setAttribute("viewBox", "50 10 200 100");
+  else if (innerWidth < 1200) $shape.setAttribute("viewBox", "50 10 30 100");
+
+  console.log("ViewBox", $shape.getAttribute("viewBox"));
   // location(Math.random(), Math.random());
 });
