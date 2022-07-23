173cf1ae462a42080b42ca93c1bac72dadab36bc
Rangel <a22110098@ceti.mx>
2022-07-22 20:53:14 -0500

    Mejoras visuales

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ $d.addEventListener("DOMContentLoaded", (e) => {
 
 $d.addEventListener("click", (e) => {
   console.log("click?");
-  clap(".clap-button", e);
+  clap(".button", e);
   sliderStudies(e);
   modalStudy(e);
   closeModal(e);
@@ -35,11 +35,19 @@ $d.addEventListener("change", (e) => {
 
 addEventListener("resize", (e) => {
   const $shape = $d.querySelector(".shape-title");
+  const $shapeMap = $d.querySelector(".shape-map");
 
-  /*if (innerHeight < 400) $shape.setAttribute("viewBox", "50 0 75 100");
-  else if (innerWidth < 600) $shape.setAttribute("viewBox", "50 10 50 100");
-  else if (innerWidth < 1000) $shape.setAttribute("viewBox", "50 10 200 100");
-  else if (innerWidth < 1200) $shape.setAttribute("viewBox", "50 10 30 100");*/
+  if (innerHeight < 400) $shape.setAttribute("viewBox", "55 20 50 100");
+  else if (innerWidth < 600) $shape.setAttribute("viewBox", "50 30 70 100");
+  else if (innerWidth < 1000) $shape.setAttribute("viewBox", "50 40 80 100");
+  else if (innerWidth < 1200) $shape.setAttribute("viewBox", "50 40 90 100");
+
+  if (innerHeight < 400) $shapeMap.setAttribute("viewBox", "-20 40 150 200");
+  else if (innerWidth < 600) $shapeMap.setAttribute("viewBox", "-15 0 150 200");
+  else if (innerWidth < 1000)
+    $shapeMap.setAttribute("viewBox", "-10 -20 150 200");
+  else if (innerWidth < 1200)
+    $shapeMap.setAttribute("viewBox", "-5 -20 150 200");
 
   console.log("ViewBox", $shape.getAttribute("viewBox"));
   // location(Math.random(), Math.random());
